9ca830c6fd74eb425269f40df00d11eac46fa9de
CsYakamoz <532609418@qq.com>
2025-06-26 00:03:31 +0800

    feat(editor): new plugin - hat0uma/csvview.nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -19,4 +19,23 @@ return {
       default_command = "im-select",
     },
   },
+
+  {
+    "hat0uma/csvview.nvim",
+    ---@module "csvview"
+    ---@type CsvView.Options
+    opts = {
+      parser = { comments = { "#", "//", "--" } },
+      view = { display_mode = "border", header_lnum = 1 },
+      keymaps = {
+        textobject_field_inner = { "if", mode = { "o", "x" } },
+        textobject_field_outer = { "af", mode = { "o", "x" } },
+        jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+        jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+        jump_next_row = { "<Enter>", mode = { "n", "v" } },
+        jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
+      },
+    },
+    cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
+  },
 }
