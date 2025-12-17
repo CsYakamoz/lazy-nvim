41bd14ea0f6a4dd239a963a414db7ddbb2660be9
CsYakamoz <532609418@qq.com>
2025-12-17 21:34:06 +0800

    feat(ui): new colorscheme - rose-pine

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -22,9 +22,18 @@ return {
   },
 
   {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      dark_variant = "moon",
+    },
+  },
+
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
+      -- colorscheme = "rose-pine",
     },
   },
 
